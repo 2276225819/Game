7c9fa5f6d8adae77eada38f7a7d14423860c2abd
2276225819 <2276225819@qq.com>
2014-10-08 10:35:37 +0800

    e

--- Classes/StartLayer.cpp
+++ Classes/StartLayer.cpp
@@ -10,16 +10,17 @@
 
 bool StartLayer::init()
 {
-    auto sb1= LayerColor::create(Color4B(255, 255, 255, 255), 80, 25);
-    sb1->setPosition(80,25);
-    auto sb2= LayerColor::create(Color4B(255, 255, 255, 255), 80, 80);
-    sb2->setPosition(80,25);
-    auto menu=Menu::
-    create(MenuItemSprite::create(sb1, sb1, [](Ref* r) {
+    auto l1= LayerColor::create(Color4B(255, 255, 255, 255), 80, 25);
+    auto sb1=  MenuItemSprite::create(l1, l1, [](Ref* r) {
         MessageBox("1", "3");
-    }), MenuItemSprite::create(sb2, sb2, [](Ref* r)  {
+    });
+    sb1->setPosition(130,25);
+    auto l2=LayerColor::create(Color4B(255, 255, 255, 255), 80, 25);
+    auto sb2=MenuItemSprite::create(l2, l2, [](Ref* r)  {
         MessageBox("2", "3");
-    }) , NULL);
+    }) ;
+    sb2->setPosition(10,25);
+    auto menu=Menu:: create(sb1,sb2 , NULL);
     addChild(menu);
     
     
