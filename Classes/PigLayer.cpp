3a23d2299b297d5bea03bbaf7093f251776f69d2
bin8673853 <2276225819@qq.com>
2014-10-08 16:38:21 +0800

    rrr

--- Classes/PigLayer.cpp
+++ Classes/PigLayer.cpp
@@ -18,10 +18,5 @@ void PigLayer::addRndPig()
     int Fx = arc4random()%8;
     auto p = Pig::createPig(Fx);
     this->addChild(p);
-     p->Run();
-}
-
-void update(float f)
-{ 
-   
+    p->Run();
 }
