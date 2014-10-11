9534b2809156d6160592162654d85b55d708a0f2
bin8673853 <2276225819@qq.com>
2014-10-11 15:16:56 +0800

    rr

--- Classes/HardMode.cpp
+++ Classes/HardMode.cpp
@@ -19,7 +19,7 @@ bool HardMode:: init()//游戏模式二：疯狂模式
         ActionInterval* ac;
     ac=Sequence::create(CallFunc::create([&]{
         pigs->addRndPig();
-    }),DelayTime::create(1), NULL);
+    }),DelayTime::create(2), NULL);
     ac=RepeatForever::create(ac);
     runAction(ac);
     
